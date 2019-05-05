2c94f67f235327f8981306ac5256b1fc6497e4a5
BITZQ <shjzhzhangqi@163.com>
2019-05-05 22:15:16 +0800

    增加新增功能

--- src/components/userList.vue
+++ src/components/userList.vue
@@ -9,7 +9,7 @@
           <el-button type="primary" v-on:click="getUsers">查询</el-button>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="handleAdd">新增</el-button>
+          <el-button type="primary" @click="createVisible=true">新增</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -67,6 +67,37 @@
         layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="20" :total="total" style="float:right">
       </el-pagination>
     </el-col>
+
+    <el-dialog :visible.sync="createVisible">
+      <el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="addForm.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="性别">
+          <el-radio-group v-model="addForm.sex">
+            <el-radio class="radio" :label="1">男</el-radio>
+            <el-radio class="radio" :label="0">女</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="年龄">
+          <el-input-number v-model="addForm.age" :min="0" :max="200"></el-input-number>
+        </el-form-item>
+        <el-form-item label="生日">
+          <el-date-picker type="date" placeholder="选择日期" v-model="addForm.birth"></el-date-picker>
+        </el-form-item>
+        <el-form-item label="地址">
+          <el-input type="textarea" v-model="addForm.addr"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click.native="addFormVisible = false">取消</el-button>
+        <el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog :visible.sync="editVisible">
+    </el-dialog>
+
   </section>
 </template>
 
@@ -84,8 +115,23 @@
             sels: [],//列表选中列
             filters: {
               name: ''
+            },
+            createVisible:false,
+            addLoading: false,
+            editVisible:false,
+            addFormRules: {
+              name: [
+                { required: true, message: '请输入姓名', trigger: 'blur' }
+              ]
+            },
+            //新增界面数据
+            addForm: {
+              name: '',
+              sex: -1,
+              age: 0,
+              birth: '',
+              addr: ''
             }
-
           }
       },
       methods:{
@@ -156,6 +202,32 @@
           }).catch(() => {
 
           });
+        },
+        addSubmit:function() {
+          this.$refs.addForm.validate((valid) => {
+            if (valid) {
+              this.$confirm('确认提交吗？', '提示', {}).then(() => {
+                this.addLoading = true;
+                //NProgress.start();
+                let para = Object.assign({}, this.addForm);
+                para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
+                addUser(para).then((res) => {
+                  this.addLoading = false;
+                  //NProgress.done();
+                  this.$message({
+                    message: '提交成功',
+                    type: 'success'
+                  });
+                  this.$refs['addForm'].resetFields();
+                  this.addFormVisible = false;
+                  this.getUsers();
+                });
+              });
+            } else {
+              return false;
+            }
+          });
+
         }
       },
       mounted:function(){
